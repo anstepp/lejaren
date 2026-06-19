dc6311c840694d263b8fa8379230ae7d0c3f77d9
anstepp <stepp.aaron@gmail.com>
2026-06-19 12:11:38 -0500

    tests set/files touched

---
+++ lejaren/intake/import_notes.py
@@ -0,0 +1,11 @@
+from lejaren.notation import Note
+
+from lejaren.intake.import_musicxml import inputParser
+
+class noteIntake:
+
+    def __init__(self):
+        self.Note = Note()
+
+    def _convert_to_note(self):
+        pass
---
+++ tests/test_import_notes.py
@@ -0,0 +1,15 @@
+import pytest
+
+from lejaren.intake.import_notes import noteIntake
+
+def test_note_create():
+    pass
+
+def test_note_duration():
+    pass
+
+def test_note_pitch():
+    pass
+
+def test_set_ticks():
+    pass
