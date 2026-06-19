264947da19599497a53c7261891d88cd70941ffe
anstepp <stepp.aaron@gmail.com>
2026-06-19 15:29:27 -0500

    fix versionitis

--- lejaren/intake/import_notes.py
+++ lejaren/intake/import_notes.py
@@ -4,8 +4,12 @@ from lejaren.intake.import_musicxml import inputParser
 
 class noteIntake:
 
-    def __init__(self):
-        self.Note = Note()
+    def __init__(self, parser=None, import_file=None):
+        if not parser:
+            self.parser = inputParser(import_file)
+        else:
+            self.parser = parser
+        self.Note = None # define now; must create note later
 
     def _convert_to_note(self):
         pass
--- tests/test_import_notes.py
+++ tests/test_import_notes.py
@@ -2,8 +2,20 @@ import pytest
 
 from lejaren.intake.import_notes import noteIntake
 
-def test_note_create():
-    pass
+@pytest.fixture
+def sample_files(scope='function'):
+    sample_files = [                  
+                "test_files/test_treble_middle_c_full_score.musicxml",
+                "test_files/test_treble_middle_c.mxl",       
+                "test_files/test_treble_middle_c_full_score.mxl",
+                "test_files/test_treble_middle_c.musicxml"
+                ]
+    return sample_files
+
+def test_note_create(sample_files):
+    for file in sample_files:
+        assert noteIntake(import_file=file)
+
 
 def test_note_duration():
     pass
